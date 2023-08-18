1d3aa4b52a2da888193ab1be477154529dda12da
Pintuprusty96 <102073531+Pintuprusty96@users.noreply.github.com>
2023-08-18 20:16:07 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,10 @@
+const acount = 1333;
+
+    let actemail = "intu";
+
+
+var acntpass = "123d"
+// acount = 21;
+// actemail = "intu"
+acntpass = 221
+console.log([acount , actemail, acntpass])
---
+++ basic/dtatype.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all js code newer version
+
+// alert( 3 + 9) // we are using node.js not browser
+
+console.log( 2 + 3) // code readability should be high
+
+
+
+let name = "Pintu Prusty";
+
+let age = 13;
+let isLoggedIn = false;
+
+let state;
+
+// number => // capacity of js 2 to the power 53
+// bigint // it is use for big data if you want whenever
+
+// string => ""
+// Boolean => true / false;
+// undefined => is output undefined
+// null => use for standalone value and it is output for object 
+
+// object 
+console.log( typeof undefined) // undefined;
+console.log( typeof null) // object
